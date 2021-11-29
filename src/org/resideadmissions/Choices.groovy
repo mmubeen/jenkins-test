ae3fd9accc4505631bc33b7405004f9edc2d556b
Mohammed Mubeen <mmubeen@resideadmissions.com>
2021-11-29 04:59:33 -0600

    wip

--- src/org/resideadmissions/Choices.groovy
+++ src/org/resideadmissions/Choices.groovy
@@ -8,11 +8,11 @@ class Choices implements Serializable{
     }
 
     def getVersions(){
-        // this.steps.withAWS(credentials:"aws-reside-dev-credentials",region:"us-east-2"){
-        //     pipeine_helper = new PipelineHelper(this.steps)
-        //     return pipeine_helper.getImageTags("us-east-2")
-        // }
-        return ['1','2','3'].join('\n')
+        this.steps.withAWS(credentials:"aws-reside-dev-credentials",region:"us-east-2"){
+            pipeine_helper = new PipelineHelper(this.steps)
+            return pipeine_helper.getImageTags("us-east-2")
+        }
+        // return ['1','2','3'].join('\n')
     }
 
     def renderVersionChoices(){
